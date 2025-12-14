f03ec7217903468d568df236b4d1e6f91d87dd39
ProceduralTree <74400509+ProceduralTree@users.noreply.github.com>
2025-12-14 23:24:06 +0100

    Update CUDA_PATH to use cudaPackages

--- fhs.nix
+++ fhs.nix
@@ -171,7 +171,7 @@ let
   '';
 
   nvidia_envvars = ''
-    export CUDA_PATH=${pkgs.cudatoolkit_11}
+    export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
     export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackackes.cudnn}/lib:${pkgs.cudaPackages.cudatoolkit.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH
     export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
   '';
